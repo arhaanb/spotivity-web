917d0d8f60aa24d26b375961960897e4e73913c6
Arhaan Bahadur <arhaanb@gmail.com>
2021-11-29 20:02:22 +0530

    penis

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<>
+			<Head>
+				<title>Spotivity</title>
+			</Head>
+
+			<div>
+				<h1>Spotivity</h1>
+			</div>
+		</>
+	)
+}
+
+export default MyApp
---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+const Index = ({ pets }) => (
+	<>
+		<h1>Spotivity</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae adipisci
+			recusandae aperiam ducimus quae voluptatum maxime, commodi saepe
+			repellendus non. Assumenda iste laboriosam quod eveniet deleniti sapiente
+			sed expedita reprehenderit.
+		</p>
+	</>
+)
+
+export default Index
