e387fc3563dcec21e01f62432bae20ff832396ba
Arhaan Bahadur <arhaanb@gmail.com>
2021-12-09 22:31:04 +0530

    analytics

--- pages/users.jsx
+++ pages/users.jsx
@@ -30,7 +30,7 @@ export default function Blog() {
         <br />
         <div className="row">
           <div className="six columns">
-            <h2 className="zero">Spotivity</h2>
+            <h2 className="zero">Spotivity Analytics</h2>
             <p>Enter your password to check Spotivity analytics. Download the mobile app from the <a href="//arhn.us/spotivity" target="_blank">Google Play Store</a>.</p>
           </div>
           <div className="u-cf"></div>
